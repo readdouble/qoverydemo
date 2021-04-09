be89d323e2ac434af15fcfb08e7e95adb7d811d7
readdouble <benjamin.arezki@gmail.com>
2021-04-09 08:39:31 +0200

    nodejs hello world

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  console.log("coucou");
+  const { Client } = require('pg');
+  /*
+
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'testdb',
+        password: '1234abcd',
+        port: 5432,
+    });
+
+client.connect();
+*/
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
