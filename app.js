a49570788b7bbe0937b1a6ef7646c31b3cfd4cc5
readdouble <benjamin.arezki@gmail.com>
2021-04-11 23:26:54 +0200

    create infra version

--- app.js
+++ app.js
@@ -5,19 +5,6 @@ const port = 3001
 app.get('/', (req, res) => {
   res.send('Hello World!');
   console.log("coucou");
-  const { Client } = require('pg');
-  /*
-
-    const client = new Client({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'testdb',
-        password: '1234abcd',
-        port: 5432,
-    });
-
-client.connect();
-*/
 })
 
 app.listen(port, () => {
