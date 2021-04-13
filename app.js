134d285d55b077ab415bd417194d02d5f8d3577b
readdouble <benjamin.arezki@gmail.com>
2021-04-13 21:52:54 +0200

    res.send

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const port = 3001
 app.get('/', (req, res) => {
   res.send('Hello World!');
   console.log("coucou");
-  console.log('The value of PORT is:', process.env.PORT);
-  console.log('The value of QOVERY_DATABASE_MY_MONGODB_NAME is:', process.env.QOVERY_DATABASE_MY_MONGODB_NAME);
+  res.send('The value of PORT is:', process.env.PORT);
+  res.send('The value of QOVERY_DATABASE_MY_MONGODB_NAME is:', process.env.QOVERY_DATABASE_MY_MONGODB_NAME);
 })
 
 app.listen(port, () => {
